abc05ebc9aba84c8292a4efb933da0b99d30c1b9
administrator <administrator@localhost.localdomain>
2019-09-28 02:35:49 +0800

    3

--- hello.cpp
+++ hello.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 
-    vector<string> msg {"Hello2", "C++", "from", "VS Code!"};
+    vector<string> msg {"Hello2", "C++", "from", "VS Code! (centOS7)"};
 
     for (const string& word : msg)
     {
